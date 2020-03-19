1de828cdec1a993eb88044afb19eab42c79d8062
Amina Amraoui <amraoui.amina@gmail.com>
2020-03-19 15:52:30 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/analytics'
 
 
 const config ={
-    apiKey: "AIzaSyDbB9kXs0zLTBzyd4qFKM_ahsii24YqZek",
-    authDomain: "crm-app-20e1a.firebaseapp.com",
-    databaseURL: "https://crm-app-20e1a.firebaseio.com",
-    projectId: "crm-app-20e1a",
-    storageBucket: "crm-app-20e1a.appspot.com",
-    messagingSenderId: "236042662431",
-    appId: "1:236042662431:web:300c31044e871933eddb2a",
-    measurementId: "G-FQ13MT426D"
+    apiKey: " ",
+    authDomain: " ",
+    databaseURL: " ",
+    projectId: " ",
+    storageBucket: " ",
+    messagingSenderId: " ",
+    appId: "",
+    measurementId: ""
 }
 
 firebase.initializeApp(config)
